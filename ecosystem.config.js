053b49436a20ece881134423d8cb073467d2d6d1
muffid <muffid.mahnun@gmail>
2024-06-10 19:31:13 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: 'rabine',
+        script: 'npm',
+        args: 'start',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
+  
