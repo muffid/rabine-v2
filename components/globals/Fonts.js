4817ce26e3f2ce62840f44ce8db8e66a3659ca8e
muffid mahnun <muffid.mahnun@gmail.com>
2025-01-12 19:39:23 +0700

    add lukim rabine

--- components/globals/Fonts.js
+++ components/globals/Fonts.js
@@ -1,6 +1,11 @@
-import { Cinzel_Decorative, Damion, Stalemate, Forum } from 'next/font/google'
+import { Cinzel_Decorative, Damion, Stalemate, Forum, Poppins } from 'next/font/google'
  
 const cinzel = Cinzel_Decorative({
+ weight:['400','700','900'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+const poppins = Poppins({
  weight:['400','700','900'],
   subsets: ['latin'],
   display: 'swap',
@@ -24,4 +29,6 @@ const forum = Forum({
   display: 'swap',
 })
 
-export {cinzel,damion,stalemate,forum}
+
+
+export {cinzel,damion,stalemate,forum,poppins}
---
+++ components/preset/presA06/Timer.js
@@ -0,0 +1,91 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import { useState, useEffect,useRef} from 'react'
+import { FaCalendarAlt, FaRegCalendarPlus } from "react-icons/fa"
+const Timer = (props) => {
+  const propsTarget = props.targetDate
+  const fadeUp = props.anim.fadeUp
+  const bgImage = props.bgImage
+  const url = props.url
+  const [timeRemaining, setTimeRemaining] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
+
+  const saveDate = () => {
+    window.open(url, '_blank');
+  }
+
+  useEffect(() => {
+    const targetDate = new Date(propsTarget);
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const difference = targetDate - now;
+
+    
+      
+      if (difference <= 0) {
+        clearInterval(intervalId);
+        setTimeRemaining({
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0
+        });
+      } else {
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+        
+        setTimeRemaining({
+          days,
+          hours,
+          minutes,
+          seconds
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [propsTarget]);
+  return (
+    <div className='w-full flex flex-col items-center justify-center bg-cover bg-center py-6 px-12 gap-y-8 text-slate-800 relative'>
+        {/* <Image
+          src={bgImage}
+          fill
+          style={{objectFit:"cover"}}
+          className='absolute'
+          sizes="100vw"
+        /> */}
+        <h1 className={'text-2xl  '+fadeUp}>SAVE THE DATE</h1>
+         <div className={'grid grid-cols-4 gap-8 '+fadeUp}>
+            <div className='flex flex-col items-center justify-center'>
+              <h1 className='text-2xl '>{timeRemaining.days}</h1>
+              <p className='text-sm'>Hari</p>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+              <h1 className='text-2xl '>{timeRemaining.hours}</h1>
+              <p className='text-sm'>Jam</p>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+              <h1 className='text-2xl '>{timeRemaining.minutes}</h1>
+              <p className='text-sm'>Menit</p>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+              <h1 className='text-2xl '>{timeRemaining.seconds}</h1>
+              <p className='text-sm'>Detik</p>
+            </div>
+          </div>
+          <button onClick={saveDate} className='text-white fadeUp flex flex-row items-center justify-center text-sm rounded-full gap-2 border bg-[#94A7C0] border-white px-4 py-2 mt-8
+                              hover:bg-white hover:text-black hover:scale-105 transition-all ease-out fadeUp'>
+            <FaCalendarAlt/> Buat Pengingat
+          </button>
+      </div>
+  )
+}
+
+export default Timer
